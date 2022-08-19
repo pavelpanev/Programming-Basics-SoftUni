f8c7cf746a5ae29bcc6117b825cd19f687bc0f6a
pavelpanev <106478398+pavelpanev@users.noreply.github.com>
2022-08-19 15:57:35 +0300

    Added a solution

---
+++ 01. First Steps/Lab/pet_shop.py
@@ -0,0 +1,6 @@
+dogfood_price = 2.5
+cat_food_price = 4
+dog_food = int(input())
+cat_food = int(input())
+whole_price = dog_food * dogfood_price + cat_food * cat_food_price
+print(f"{whole_price} lv.")
