98adb14ee19f2ce318b68ee24ffb20f89f2f6aec
pavelpanev <106478398+pavelpanev@users.noreply.github.com>
2022-08-19 16:00:03 +0300

    Added solutions

---
+++ 01. First Steps/Lab/projects_creation.py
@@ -0,0 +1,4 @@
+architect_name = input()
+project_num = int(input())
+project_time = project_num * 3
+print(f"The architect {architect_name} will need {project_time} hours to complete {project_num} project/s.")
---
+++ 01. First Steps/Lab/yard_greening.py
@@ -0,0 +1,6 @@
+sq_meters = float(input())
+price = sq_meters * 7.61
+discount = price * 0.18
+new_price = price - discount
+print(f"The final price is: {new_price} lv.")
+print(f"The discount is: {discount} lv.")
