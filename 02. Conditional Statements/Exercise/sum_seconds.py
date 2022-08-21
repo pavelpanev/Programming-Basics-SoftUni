42a1b668e2d5a834fd01198ecb76819241fefa6d
pavelpanev <106478398+pavelpanev@users.noreply.github.com>
2022-08-21 20:04:03 +0300

    Solution to Sum Seconds

---
+++ 02. Conditional Statements/Exercise/sum_seconds.py
@@ -0,0 +1,10 @@
+time_1 = int(input())
+time_2 = int(input())
+time_3 = int(input())
+total_time = time_3 + time_2 + time_1
+minutes = int(total_time // 60)
+seconds = total_time % 60
+if seconds < 10:
+    print(f"{minutes}:0{seconds}")
+else:
+    print(f"{minutes}:{seconds}")
