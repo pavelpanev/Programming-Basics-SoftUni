df07336d4fe42fc5820bdee477360417ad16f692
pavelpanev <106478398+pavelpanev@users.noreply.github.com>
2022-08-26 20:12:38 +0300

    Solution to Rectangle Border

---
+++ 03. Conditional Statements Advanced/More Exercises/rectangle_border.py
@@ -0,0 +1,17 @@
+x1 = float(input())
+y1 = float(input())
+x2 = float(input())
+y2 = float(input())
+x = float(input())
+y = float(input())
+# Outside
+if (x < x1 or x > x2) or (y < y1 or y > y2):
+    print("Inside / Outside")
+# Inside
+elif x1 < x < x2 and y1 < y < y2:
+    print("Inside / Outside")
+# Side-Border
+elif (x == x1 or x == x2) and y1 <= y < y2:
+    print("Border")
+elif (y == y1 or y == y2) and x1 <= x <= x2:
+    print("Border")
