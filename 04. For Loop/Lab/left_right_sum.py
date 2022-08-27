4e595c9650a7d3f653418b05e5e477304b694dc8
pavelpanev <106478398+pavelpanev@users.noreply.github.com>
2022-08-27 18:34:18 +0300

    Solution to Left and Right Sum

---
+++ 04. For Loop/Lab/left_right_sum.py
@@ -0,0 +1,15 @@
+n = int(input())
+left_sum = 0
+right_sum = 0
+for i in range(1, n + 1):
+    number = int(input())
+    left_sum += number
+
+for i in range(1, n + 1):
+    number = int(input())
+    right_sum += number
+
+if left_sum == right_sum:
+    print(f"Yes, sum = {left_sum}")
+else:
+    print(f"No, diff = {abs(left_sum - right_sum)}")
